06c84f96de479fcc78f2101ab6a59d79eacc5342
ahmadtirfas0-netizen <ahmadtirfas0@gmail.com>
2025-08-27 13:49:29 +0200

    Deleted src

--- src/pages/AuditPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AuditLogViewer from '../components/AuditLog/AuditLogViewer';
-
-const AuditPage: React.FC = () => {
-  return <AuditLogViewer />;
-};
-
-export default AuditPage;
